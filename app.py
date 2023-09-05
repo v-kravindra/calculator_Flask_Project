6da45e9bfc9e61e2c1d893f3710a411aac90aac9
Dilipkg <guptadilipdev100@gmail.com>
2023-09-05 13:58:44 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,60 @@
-from flask import Flask
+from flask import Flask, render_template, request,jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+@app.route('/', methods=['GET','POST'])
+def home_page():
+    return render_template('index.html')
+
+
+
+@app.route('/math',methods=['POST'])
+def math_ops():
+    if(request.method == 'POST'):
+        ops = request.form['operation']
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        if ops == 'add':
+            r = num1+num2
+            result = "The sum of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if (ops=='subtract'):
+            r = num1 - num2
+            result= 'the subtrsct of ' +str(num1) + 'and' + str(num2) + "is " + str(r)
+
+        if (ops=='multiply'):
+            r = num1 * num2
+            result= 'the multiply of ' +str(num1) + 'and' + str(num2) + "is " + str(r)
+
+        if (ops=='divide'):
+            r = num1 / num2
+            result= 'the divide of ' +str(num1) + 'and' + str(num2) + "is " + str(r)          
+  
+        return render_template('results.html',result=result)
+
+@app.route('/postman_data',methods=['POST'])
+def math_ops1():
+    if(request.method == 'POST'):
+        ops = request.json['operation']
+        num1 = int(request.json['num1'])
+        num2 = int(request.json['num2'])
+        if ops == 'add':
+            r = num1+num2
+            result = "The sum of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
+        if (ops=='subtract'):
+            r = num1 - num2
+            result= 'the subtrsct of ' +str(num1) + 'and' + str(num2) + "is " + str(r)
+
+        if (ops=='multiply'):
+            r = num1 * num2
+            result= 'the multiply of ' +str(num1) + 'and' + str(num2) + "is " + str(r)
+
+        if (ops=='divide'):
+            r = num1 / num2
+            result= 'the divide of ' +str(num1) + 'and' + str(num2) + "is " + str(r)          
+  
+        return jsonify(result)
+
+
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
